d98d46760fcc1964436e39875a4ab3788f8d6d2d
AngelesH23 <142943218+AngelesH23@users.noreply.github.com>
2023-08-22 15:43:23 -0600

    Cargue un boton

--- Calificaciones/Form1.Designer.cs
+++ Calificaciones/Form1.Designer.cs
@@ -40,13 +40,15 @@ namespace Calificaciones
             this.btnLimpiar = new System.Windows.Forms.Button();
             this.Nuevo = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnMostrar
             // 
-            this.btnMostrar.Location = new System.Drawing.Point(111, 296);
+            this.btnMostrar.Location = new System.Drawing.Point(83, 237);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMostrar.Name = "btnMostrar";
-            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.Size = new System.Drawing.Size(56, 24);
             this.btnMostrar.TabIndex = 0;
             this.btnMostrar.Text = "Mostrar";
             this.btnMostrar.UseVisualStyleBackColor = true;
@@ -55,35 +57,39 @@ namespace Calificaciones
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 49);
+            this.label1.Location = new System.Drawing.Point(17, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(216, 17);
+            this.label1.Size = new System.Drawing.Size(160, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ingresar el nombre del empleado";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(96, 179);
+            this.label2.Location = new System.Drawing.Point(72, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.Size = new System.Drawing.Size(31, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Total";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 118);
+            this.label3.Location = new System.Drawing.Point(20, 96);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(209, 17);
+            this.label3.Size = new System.Drawing.Size(156, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Ingrese el sueldo del empleado:";
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(239, 292);
+            this.btnSalir.Location = new System.Drawing.Point(179, 237);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 30);
+            this.btnSalir.Size = new System.Drawing.Size(56, 24);
             this.btnSalir.TabIndex = 4;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -91,31 +97,35 @@ namespace Calificaciones
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(239, 47);
+            this.txtNombre.Location = new System.Drawing.Point(179, 38);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(290, 22);
+            this.txtNombre.Size = new System.Drawing.Size(218, 20);
             this.txtNombre.TabIndex = 5;
             // 
             // txtTotal
             // 
             this.txtTotal.Enabled = false;
-            this.txtTotal.Location = new System.Drawing.Point(239, 176);
+            this.txtTotal.Location = new System.Drawing.Point(179, 143);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtTotal.Name = "txtTotal";
-            this.txtTotal.Size = new System.Drawing.Size(290, 22);
+            this.txtTotal.Size = new System.Drawing.Size(218, 20);
             this.txtTotal.TabIndex = 6;
             // 
             // txtSueldo
             // 
-            this.txtSueldo.Location = new System.Drawing.Point(239, 116);
+            this.txtSueldo.Location = new System.Drawing.Point(179, 94);
+            this.txtSueldo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSueldo.Name = "txtSueldo";
-            this.txtSueldo.Size = new System.Drawing.Size(290, 22);
+            this.txtSueldo.Size = new System.Drawing.Size(218, 20);
             this.txtSueldo.TabIndex = 7;
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Location = new System.Drawing.Point(404, 289);
+            this.btnLimpiar.Location = new System.Drawing.Point(283, 237);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(75, 30);
+            this.btnLimpiar.Size = new System.Drawing.Size(56, 24);
             this.btnLimpiar.TabIndex = 8;
             this.btnLimpiar.Text = "Limpiar";
             this.btnLimpiar.UseVisualStyleBackColor = true;
@@ -123,27 +133,41 @@ namespace Calificaciones
             // 
             // Nuevo
             // 
-            this.Nuevo.Location = new System.Drawing.Point(164, 227);
+            this.Nuevo.Location = new System.Drawing.Point(83, 184);
+            this.Nuevo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Nuevo.Name = "Nuevo";
-            this.Nuevo.Size = new System.Drawing.Size(75, 30);
+            this.Nuevo.Size = new System.Drawing.Size(56, 24);
             this.Nuevo.TabIndex = 9;
             this.Nuevo.Text = "Nuevo";
             this.Nuevo.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(298, 227);
+            this.button1.Location = new System.Drawing.Point(179, 184);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.Size = new System.Drawing.Size(56, 24);
             this.button1.TabIndex = 10;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 184);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 24);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Nuevo\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 389);
+            this.ClientSize = new System.Drawing.Size(424, 316);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Nuevo);
             this.Controls.Add(this.btnLimpiar);
@@ -155,6 +179,7 @@ namespace Calificaciones
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnMostrar);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Calificaciones";
             this.ResumeLayout(false);
@@ -175,6 +200,7 @@ namespace Calificaciones
         private System.Windows.Forms.Button btnLimpiar;
         private System.Windows.Forms.Button Nuevo;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
